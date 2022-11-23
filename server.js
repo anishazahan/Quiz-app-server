0d7e8f3018c159e8902c86ef2e4b0236d1daca6c
anishazahan <anishazahan13@gmail.com>
2022-11-23 13:38:26 +0600

    first demo api create

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
 import express from 'express';
+const app = express();
+
+
+///------route------
+
+app.get('/',(req,res)=>{
+    try {
+        res.json('get requst')
+    } catch (error) {
+        res.json(error)
+    }
+})
