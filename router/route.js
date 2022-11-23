6c87d3f3416519529ee768984c64ce30c3760e23
anishazahan <anishazahan13@gmail.com>
2022-11-23 14:18:33 +0600

    import export router file

---
+++ router/route.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+const router = Router();
+
+////---------questions routes--------
+
+
+
+export default router;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import morgan from 'morgan'
 import cors from 'cors'
 import { config } from 'dotenv';
+import router from './router/route';
 
 const app = express();
 
@@ -15,7 +16,15 @@ config();
 //-----------application port---
 const port = process.env.PORT || 5001;
 
-///------route------
+///------routes------
+
+app.use('/api',router)
+
+
+
+
+
+
 
 app.get('/',(req,res)=>{
     try {
